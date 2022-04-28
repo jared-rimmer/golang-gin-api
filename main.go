f84fce7874f47a8f0ddcda420fc1c81f7d287f95
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-04-28 19:54:17 +0100

    Add all files for basic library api

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,8 +25,102 @@ func getBooks(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, books)
 }
 
+func bookById(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		return
+	}
+
+	book, err := getBookById(int(id))
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id int) (*book, error) {
+	for i, v := range books {
+		if v.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("Book not found")
+}
+
+func createBook(context *gin.Context) {
+	var newBook book
+
+	if err := context.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	context.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func checkoutBook(context *gin.Context) {
+	id, ok := context.GetQuery("id")
+
+	if !ok {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+
+	new_id, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return
+	}
+
+	book, err := getBookById(int(new_id))
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+
+	book.Quantity -= 1
+	context.IndentedJSON(http.StatusOK, book)
+}
+
+func returnBook(context *gin.Context) {
+	id, ok := context.GetQuery("id")
+
+	if !ok {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing %v query parameter"})
+		return
+	}
+
+	new_id, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return
+	}
+
+	book, err := getBookById(int(new_id))
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	book.Quantity += 1
+	context.IndentedJSON(http.StatusOK, book)
+
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
+	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
 	router.Run("localhost:8080")
 }
